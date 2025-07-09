b4124d76db2dbdb0e165551cc72d5d19c5f53683
Denver <valerianodenver@gmail.com>
2025-07-09 12:47:04 +0800

    Add files via upload

---
+++ DenverLoue_Valeriano/sikatnamemes.py
@@ -0,0 +1,10 @@
+# QUIZ NG SIKAT Na MEMES
+print("1. Kapatid mo ba ung sikat na memes?")
+print("A. OPO)
+print("B. HINDI PO")
+answer = input("Iyong kasagutan: ")
+if answer.upper() == "A":
+    print("Tama ka!")
+    score += 1
+else:
+    print("Mali! May sapak ka sa utak.")
